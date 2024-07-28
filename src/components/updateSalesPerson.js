1418a32858389a64ad90f3e8a01df411c8db9503
akakizaki <ayakakiz2002@gmail.com>
2024-07-28 23:48:33 +0900

    front end

--- src/components/updateSalesPerson.js
+++ src/components/updateSalesPerson.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import axios from '../axios';
 
-const UpdateSalesperson = ({ salespersonId }) => {
+const UpdateSalesperson = ({ salespersonId }) => { 
   const [salesperson, setSalesperson] = useState({
     firstName: '',
     lastName: '',
